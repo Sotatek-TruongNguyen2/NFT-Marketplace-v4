594299d9ed40c393e6fdd4284f689ff79904304d
levblanc <levblanc@gmail.com>
2022-10-04 17:31:53 +0800

    Add dynamic NFT deployment scripts

---
+++ deploy/00-deploy-dynamic-nft.js
@@ -0,0 +1,47 @@
+const { network } = require('hardhat');
+const { developmentChains } = require('../helper-hardhat-config');
+const storeImagesAndMetadata = require('../utils/uploadToPinata');
+const { verify } = require('../utils/verify');
+
+const imgFilePath = '../images';
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const isLocalNetwork = developmentChains.includes(network.name);
+  // let tokenURIs = [];
+  const waitConfirmations = network.config.blockConfirmations || 1;
+
+  // Uploaded images and metadata to Pinata IPFS
+  // Use the records here and not duplicate uploading again
+  const tokenURIs = [
+    'ipfs://QmSAsimJkGUYUDeHjrYNSdeLgd5WHVT6UNP78C4Zk4pgjk',
+    'ipfs://QmYEEFbSnctc8i69oSNPVysZ11dHDAdiZ16kNMALKFhHBv',
+    'ipfs://QmQmesTuPE2X5ZnqoM4Y9jTEzX9zQmVVVFzgqvvdhpmFry',
+    'ipfs://QmNSkVVhDGwhzXFN6fHdC23hjaSx3p7nQdSiziJiY392w9',
+    'ipfs://QmVAMuux5EVmy7PBPyWDKRm4d5Rqk92VUfbtfX4GDtc7BT',
+    'ipfs://QmZ3aohHAXDmB5EudURfsVuqnQ8gcgp7Eix1fpX3KAAwUu',
+  ];
+
+  if (process.env.UPLOAD_TO_PINATA === 'true') {
+    tokenURIs = await storeImagesAndMetadata(imgFilePath);
+  }
+
+  log('-----------------------------------------');
+
+  const args = [tokenURIs];
+  const dynamicNFT = await deploy('DynamicNFT', {
+    from: deployer,
+    args,
+    log: true,
+    waitConfirmations,
+  });
+
+  if (!isLocalNetwork && process.env.ETHERSCAN_API_KEY) {
+    await verify(dynamicNFT.address, args);
+  }
+
+  log('-----------------------------------------');
+};
+
+module.exports.tags = ['all', 'dynamicNFT'];
---
+++ utils/uploadToPinata.js
@@ -0,0 +1,70 @@
+const pinataSDK = require('@pinata/sdk');
+const path = require('path');
+const fs = require('fs-extra');
+require('dotenv').config();
+
+const { PINATA_API_KEY, PINATA_API_SECRET } = process.env;
+const pinata = pinataSDK(PINATA_API_KEY, PINATA_API_SECRET);
+const metadataTemplate = {
+  name: '',
+  description: '',
+  image: '',
+  attributes: [
+    {
+      trait_type: 'Cuteness',
+      value: 100,
+    },
+  ],
+};
+
+const storeImagesAndMetadata = async (imgFilePath) => {
+  const imgDir = path.resolve(__dirname, imgFilePath);
+  const files = fs.readdirSync(imgDir);
+  const tokenURIs = [];
+
+  console.log('>>>>>> Uploading files & metadata to IPFS...');
+  console.log('>>>>>>', files);
+
+  for (index in files) {
+    const fileName = files[index];
+    const catName = fileName.split('.')[0].replace('-', ' ');
+    const filePath = path.resolve(imgDir, fileName);
+    const readableStreamForFile = fs.createReadStream(filePath);
+
+    try {
+      // Store the image in IPFS
+      const pinFileResp = await pinata.pinFileToIPFS(readableStreamForFile);
+      console.log(`>>>>>> Upload ${fileName} to IPFS success!`);
+
+      const description = `A cute ${catName} cat!`;
+      const image = `ipfs://${pinFileResp.IpfsHash}`;
+
+      const tokenURIMetadata = Object.assign({}, metadataTemplate, {
+        name: catName,
+        description,
+        image,
+      });
+
+      try {
+        // Store the metadata in IPFS
+        const pinJsonResp = await pinata.pinJSONToIPFS(tokenURIMetadata);
+
+        console.log(`>>>>>> Upload ${fileName} metadata to IPFS success!`);
+        const tokenURI = `ipfs://${pinJsonResp.IpfsHash}`;
+
+        tokenURIs.push(tokenURI);
+      } catch (error) {
+        console.log('>>>>>> Error when pinning metadata:');
+        console.error(error);
+      }
+    } catch (error) {
+      console.log('>>>>>> Error when uploading image:');
+      console.error(error);
+    }
+  }
+
+  console.log('>>>>>> token URIs', tokenURIs);
+  return tokenURIs;
+};
+
+module.exports = storeImagesAndMetadata;
